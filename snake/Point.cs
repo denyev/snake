68935ae098b7ce1abbeecfc9770ae60a88f7d4cd
denyev <denis.yevtushenko@yandex.ru>
2015-12-06 17:45:59 +0300

    Add Class Point

---
+++ snake/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace snake
+{
+	public class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+
+		public void Draw ()
+		{
+			Console.SetCursorPosition (x, y);
+			Console.Write (sym);
+		}
+	}
+}
+
